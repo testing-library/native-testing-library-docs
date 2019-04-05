d2f77b7e36dd7af572f39a5ee0ab484bd7bdc07a
Brandon Carroll <brandonvcarroll@gmail.com>
2019-04-04 22:49:15 -0400

    put footer image back

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -25,34 +25,34 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
+          <Link href={this.props.config.baseUrl} className="nav-home">
+            {this.props.config.footerIcon && (
+              <img
+                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                alt={this.props.config.title}
+                width="66"
+                height="58"
+              />
+            )}
+          </Link>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('intro', this.props.language)}>
-              Getting Started
-            </a>
-            <a href={this.docUrl('install', this.props.language)}>
-              Install
-            </a>
-            <a href={this.docUrl('example', this.props.language)}>
-              Example
-            </a>
-            <a href={this.docUrl('api-queries', this.props.language)}>
-              API
-            </a>
+            <a href={this.docUrl('intro', this.props.language)}>Getting Started</a>
+            <a href={this.docUrl('install', this.props.language)}>Install</a>
+            <a href={this.docUrl('example', this.props.language)}>Example</a>
+            <a href={this.docUrl('api-queries', this.props.language)}>API</a>
           </div>
           <div>
             <h5>Community</h5>
             <a
               href="http://stackoverflow.com/questions/tagged/native-testing-library"
               target="_blank"
-              rel="noreferrer noopener">
+              rel="noreferrer noopener"
+            >
               Stack Overflow
             </a>
             <a href="https://reactiflux.com/">Reactiflux on Discord</a>
-            <a
-              href="https://twitter.com/bcarroll22"
-              target="_blank"
-              rel="noreferrer noopener">
+            <a href="https://twitter.com/bcarroll22" target="_blank" rel="noreferrer noopener">
               Follow on Twitter
             </a>
           </div>
@@ -65,7 +65,8 @@ class Footer extends React.Component {
               data-count-href="/facebook/docusaurus/stargazers"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
+              aria-label="Star this project on GitHub"
+            >
               Star
             </a>
             <a href="https://github.com/bcarroll22/native-testing-library">GitHub</a>
