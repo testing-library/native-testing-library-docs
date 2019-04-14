bb63b4f185db0773fdb3ae07f309db2c6a397909
Brandon Carroll <brandonvcarroll@gmail.com>
2019-04-13 22:22:20 -0400

    fix links in footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -40,10 +40,10 @@ class Footer extends React.Component {
           </Link>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('intro', this.props.language)}>Getting Started</a>
-            <a href={this.docUrl('install', this.props.language)}>Install</a>
-            <a href={this.docUrl('example', this.props.language)}>Example</a>
-            <a href={this.docUrl('api-queries', this.props.language)}>API</a>
+            <a href={this.docUrl('intro')}>Getting Started</a>
+            <a href={this.docUrl('install')}>Install</a>
+            <a href={this.docUrl('example')}>Example</a>
+            <a href={this.docUrl('api-queries')}>API</a>
           </div>
           <div>
             <h5>Community</h5>
